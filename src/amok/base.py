91a01400b5978499e737ac90fd6eaf5ad8ff9776
Rahul Thomas <rapidclock@users.noreply.github.com>
2025-07-16 21:14:51 -0700

    v0.1.1 - fix ssl_verify

--- src/amok/base.py
+++ src/amok/base.py
@@ -4,6 +4,7 @@ import re
 from abc import ABC, abstractmethod
 from typing import Any, Self
 
+import httpx
 from openai import OpenAI
 from openai.types.chat import (
     ChatCompletion,
@@ -35,16 +36,18 @@ class BaseAgent(ABC):
             settings: The settings for the agent.
 
         """
-        self.openai_client = OpenAI(
-            base_url=settings.base_url,
-            api_key=settings.api_key,
-        )
         self.model = settings.model
         self.temperature = settings.temperature
         self.max_tokens = settings.max_tokens
         self.ssl_verify = settings.ssl_verify
         self.is_thinking_agent = settings.thinking_mode
         self.stream = False
+        http_client = httpx.Client(verify=settings.ssl_verify)
+        self.openai_client = OpenAI(
+            base_url=settings.base_url,
+            api_key=settings.api_key,
+            http_client=http_client,
+        )
 
     @classmethod
     def from_cfg(cls, cfg_file_path: str) -> Self:
